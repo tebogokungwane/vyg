c989f4165aa94ac91f009e8f016d6b248894f39e
F4933702 <f4933702@fnb.co.za>
2026-05-29 22:26:43 +0200

    adding render url

--- src/main/java/com/vyg/config/CorsConfig.java
+++ src/main/java/com/vyg/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
 
         config.setAllowedOriginPatterns(List.of(
                 "http://localhost:3000",
-                "http://127.0.0.1:3000"
+                "http://127.0.0.1:3000",
+                "https://vyg-front-end.onrender.com"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
--- src/main/java/com/vyg/config/security/SecurityConfig.java
+++ src/main/java/com/vyg/config/security/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
